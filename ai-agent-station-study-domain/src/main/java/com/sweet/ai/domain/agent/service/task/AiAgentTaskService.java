59b65e6b5b8c674744756d6cb99761cba5cf2722
gaoshou45 <477591620@qq.com>
2025-12-11 10:07:56 +0800

    feat: 动态执行智能体任务

--- ai-agent-station-study-domain/src/main/java/com/sweet/ai/domain/agent/service/task/AiAgentTaskService.java
+++ ai-agent-station-study-domain/src/main/java/com/sweet/ai/domain/agent/service/task/AiAgentTaskService.java
@@ -3,10 +3,12 @@ package com.sweet.ai.domain.agent.service.task;
 import com.sweet.ai.domain.agent.adapter.repository.IAgentRepository;
 import com.sweet.ai.domain.agent.model.valobj.AiAgentTaskScheduleVO;
 import com.sweet.ai.domain.agent.service.ITaskService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Service
 public class AiAgentTaskService implements ITaskService {
 
     @Resource
